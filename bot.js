58d55d05188afc5a980932447de4bf2cef17cc90
Afdaser <98528117+Afdaser@users.noreply.github.com>
2022-02-01 10:42:15 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const { Telegraf, Markup } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const keyboard = Markup.inlineKeyboard([
-    Markup.urlButton(process.env.ONE_BUTTON, process.env.ONE_LINK),
-    Markup.urlButton(process.env.TWO_BUTTON, process.env.TWO_LINK),
-]);
-
-bot.start((ctx) =>
-    ctx.telegram.sendMessage(ctx.chat.id,process.env.GREETINGS,{ reply_markup: keyboard, parse_mode: "MarkdownV2" })
+bot.start((ctx) => ctx.reply(process.env.GREETINGS,
+        Markup.keyboard([
+            [process.env.ONE_BUTTON, process.env.TWO_BUTTON],
+        ])
+            .resize()
+    )
 );
+
+bot.hears(process.env.ONE_BUTTON, (ctx) => ctx.reply(process.env.ONE_LINK))
+bot.hears(process.env.TWO_BUTTON, (ctx) => ctx.reply(process.env.TWO_LINK))
+
+
 bot.launch();
 
 
